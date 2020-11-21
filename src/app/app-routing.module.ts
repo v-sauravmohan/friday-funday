0ad99de3a3c623cc7853c8769d8b229412283247
gokul-prathap <67264345+gokul-prathap@users.noreply.github.com>
2020-11-22 01:25:41 +0530

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { RedeemPointsPageComponent } from './pages/redeem-points-page/redeem-points-page.component';
+import { ScorePageComponent } from './pages/score-page/score-page.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: 'score-page',
+    component: ScorePageComponent,
+  },
+  {
+    path:'redeem',
+    component:RedeemPointsPageComponent
+  },
+  {
+    path: '**',
+    redirectTo: 'home'
+  }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
