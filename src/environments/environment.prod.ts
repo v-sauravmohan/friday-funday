da17fd2be50e7b9b6a561c8df27ed79b1f5da29d
Saurav Mohan V <sauravmohan2@gmail.com>
2020-11-26 16:21:09 +0530

    Deployment Files for Hosting

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAa11LN6fDMOPAXrDGfBpJ16WJVtMepyqA',
+    authDomain: 'friday-funday.firebaseapp.com',
+    databaseURL: 'https://friday-funday.firebaseio.com',
+    projectId: 'friday-funday',
+    storageBucket: 'friday-funday.appspot.com',
+    messagingSenderId: '45040277938',
+    appId: '1:45040277938:web:35a231bf5be0e261552cab'
+  }
 };
